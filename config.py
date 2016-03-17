db46160e893fc2e2f68d34cceab79b16647be20f
Alexander <alexhaarberg@hotmail.com>
2016-03-17 11:55:28 +0100

    started working on game-class

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 
-import pygame
-import Vector2D
-
 from main import *
 
 SCREEN_X, SCREEN_Y = 1200, 720
+GAME_SCALE = SCREEN_X // 100
 
 GRAVITY = 10
 MOVE_SPEED = 10
 TURN_SPEED = 10
 
+P1_FNAME = "img/p1.png"
 P1_L = pygame.K_a
 P1_R = pygame.K_d
 P1_F = pygame.K_w
 P1_S = pygame.K_f
 
+P2_FNAME = "img/p2.png"
 P2_L = pygame.K_j
 P2_R = pygame.K_l
 P2_F = pygame.K_i
--- main.py
+++ main.py
@@ -10,9 +10,39 @@ from weapons import *
 
 
 class game():
-    pass
+    def __init__(self):
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((SCREEN_X, SCREEN_Y))
+
+    def run(self):
+        self.clock = pygame.time.Clock()
+        
+        while True:
+            self.time_passed = self.clock.tick(60)
+            self.time_passed_seconds = self.time_passed / 1000.0
+
+            self.handle_event()
+            self.handle_move()
+            self.handle_draw()
+
+    def handle_event(self):
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                exit()
+
+    def handle_move(self):
+        pass
+
+    def handle_draw(self):
+        self.screen.fill((32,32,40))
+
+
+        pygame.display.flip()
 
 
 
 if __name__ == '__main__':
-    pass
+    spill = game()
+    spill.run()
